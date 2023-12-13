2a1d670f44d5f8bb45090ae5010517d689f8de1f
Loginov Alexey <loginov.a@eremex.ru>
2023-12-14 00:44:21 +0300

    Can draw and erase many rectangles and lines.

--- front/src/pages/Main.vue
+++ front/src/pages/Main.vue
@@ -1,7 +1,10 @@
 <template>
 	<h2>{{ message }}</h2>
-	<canvas @mousedown="startPainting" @mouseup="finishedPainting" @mousemove="draw" @mouseleave="finishedPainting" id="canvas"></canvas>
 	
+	<div id="wrapper">
+		<canvas @mousedown="startPainting" @mouseup="finishedPainting" @mousemove="drawing" id="canvas1"></canvas>
+		<canvas @mouseup="finishedPaintingRect" @mouseout="finishedPaintingRect" @mousemove="drawingRect" id="canvas2"></canvas>
+	</div>
 	<div class="color-picker">
 		<div v-for="color in colors" class="color-box" :style="{ backgroundColor: color }" @click="changeColor(color)"></div>
 	</div>
@@ -11,20 +14,29 @@
 		<a class="tool-button" @click.prevent="toggleEraserMode">{{eraserModeText}}</a>
 		<a class="tool-button" @click.prevent="clearCanvas">🗑️</a>
 	</div>
-	<a class="clear-button">LONG BUTTON</a>
+	<!-- <a class="clear-button">LONG BUTTON</a> -->
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 const message = ref("Drawing App");
 const painting = ref(false);
-const canvas = ref(null);
-const ctx = ref(null);
+const wrapper = ref(null);
+const canvas1 = ref(null);
+const canvas2 = ref(null);
+const offsetX = ref(0);
+const offsetY = ref(0);
+const scaleX = ref(1);
+const scaleY = ref(1);
+const ctx1 = ref(null);
+const ctx2 = ref(null);
 const colors = ref(["#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"]);
 const changeColor = (color) => {
 	if(eraserMode.value==false){
-		ctx.value.strokeStyle = color;
-		ctx.value.fillStyle = color;
+		ctx1.value.strokeStyle = color;
+		ctx1.value.fillStyle = color;
+		ctx2.value.strokeStyle = color;
+		ctx2.value.fillStyle = color;
 	}
 	lastColor.value = color;
 };
@@ -42,72 +54,129 @@ const lastColor = ref("#000000");
 const toggleEraserMode = () => {
 	eraserMode.value = !eraserMode.value;
 	eraserModeText.value = eraserMode.value == true?'🔲':'🔳';
-	ctx.value.strokeStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
-	ctx.value.fillStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
+	ctx1.value.strokeStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
+	ctx1.value.fillStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
+	ctx2.value.strokeStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
+	ctx2.value.fillStyle = eraserMode.value == true?"#FFFFFF":lastColor.value;
+	ctx1.value.globalCompositeOperation = eraserMode.value == true?"destination-out":"source-over";
 };
 
 const clearCanvas = () => {
-	ctx.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
+	ctx1.value.clearRect(0, 0, canvas1.value.width, canvas1.value.height);
+	ctx2.value.clearRect(0, 0, canvas2.value.width, canvas2.value.height);
 };
 
 const rectStartX=ref(0);
 const rectStartY=ref(0);
 
+const startPaintingRect = (e) => {
+	if(rectangleMode.value==true){
+		rectStartX.value = (e.clientX - offsetX.value)/scaleX.value;
+		rectStartY.value = (e.clientY - offsetY.value)/scaleY.value;
+		ctx2.value.clearRect(0,0,canvas2.value.width,canvas2.value.height);
+		canvas2.value.style.left = "0%";
+		painting.value = true;
+	}
+}
+
 const startPainting = (e) => {
-	painting.value = true;
+	startPaintingRect(e);
+	if(rectangleMode.value==false){
+		painting.value = true;
+		drawing(e);
+	}
+};
+const finishedPaintingRect = (e) => {
 	if(rectangleMode.value==true){
-		rectStartX.value = e.clientX - canvas.value.offsetLeft;
-		rectStartY.value = e.clientY - canvas.value.offsetTop;
+		const newMouseX = (e.clientX - offsetX.value)/scaleX.value;
+		const newMouseY = (e.clientY - offsetY.value)/scaleY.value;
+		painting.value = false;
+		canvas2.value.style.left = "-200%";
+		drawRect(newMouseX,newMouseY,ctx1);
+	}
+}
+
+const finishedPainting = (e) => {
+	const newMouseX = (e.clientX - offsetX.value)/scaleX.value;
+	const newMouseY = (e.clientY - offsetY.value)/scaleY.value;
+	if(rectangleMode.value==true){
+		painting.value = false;
+		canvas2.value.style.left = "-200%";
+		drawRect(newMouseX,newMouseY,ctx1);
 	}
 	else{
-		draw(e);
+		painting.value = false;
+		ctx1.value.beginPath();
 	}
 };
 
-const finishedPainting = () => {
-	painting.value = false;
-	if(rectangleMode.value==true){
+const drawRect = (toX,toY,context) => {
+	context.value.beginPath();
+	context.value.fillRect(rectStartX.value,rectStartY.value,toX-rectStartX.value,toY-rectStartY.value);
+	context.value.stroke();
+}
+const drawLine = (toX,toY,context) => {
+	context.value.lineTo(toX, toY);
+	context.value.stroke();
 
-	}
-	else{
-		ctx.value.beginPath();
-	}
-};
+	context.value.beginPath();
+	context.value.moveTo(toX, toY);
+}
 
-const draw = (e) => {
+const drawingRect = (e) => {
 	if (!painting.value) return;
-	ctx.value.lineWidth = 10;
-	ctx.value.lineCap = "round";
 	if(rectangleMode.value==true){
-		const newMouseX = e.clientX - canvas.value.offsetLeft;
-		const newMouseY = e.clientY - canvas.value.offsetTop;
-
-		const rectWidth = newMouseX - rectStartX.value;
-		const rectHeight = newMouseY - rectStartY.value;
-
-		ctx.value.clearRect(0,0,canvas.value.width,canvas.value.height)
-		ctx.value.fillRect(rectStartX.value,rectStartY.value,rectWidth,rectHeight);
+	ctx1.value.lineWidth = 5;
+	ctx1.value.lineCap = "round";
+	ctx2.value.lineWidth = 5;
+	ctx2.value.lineCap = "round";
+	const newMouseX = (e.clientX - offsetX.value)/scaleX.value;
+	const newMouseY = (e.clientY - offsetY.value)/scaleY.value;
+		ctx2.value.clearRect(0,0,canvas2.value.width,canvas2.value.height);
+		drawRect(newMouseX,newMouseY,ctx2);
 	}
-	else{
-		ctx.value.lineTo(e.clientX - canvas.value.offsetLeft, e.clientY - canvas.value.offsetTop);
-		ctx.value.stroke();
+}
 
-		ctx.value.beginPath();
-		ctx.value.moveTo(e.clientX - canvas.value.offsetLeft, e.clientY - canvas.value.offsetTop);
+const drawing = (e) => {
+	if (!painting.value) return;
+	ctx1.value.lineWidth = 5;
+	ctx1.value.lineCap = "round";
+	ctx2.value.lineWidth = 5;
+	ctx2.value.lineCap = "round";
+	const newMouseX = (e.clientX - offsetX.value)/scaleX.value;
+	const newMouseY = (e.clientY - offsetY.value)/scaleY.value;
+	if(rectangleMode.value==false){
+		drawLine(newMouseX,newMouseY,ctx1);
 	}
 };
 
 onMounted(() => {
-	canvas.value = document.getElementById("canvas");
-	ctx.value = canvas.value.getContext("2d");
+	wrapper.value = document.getElementById("wrapper");
+	offsetX.value += wrapper.value.offsetLeft;
+	offsetY.value += wrapper.value.offsetTop;
+	canvas1.value = document.getElementById("canvas1");
+	offsetX.value += canvas1.value.offsetLeft;
+	offsetY.value += canvas1.value.offsetTop;
+	scaleX.value = canvas1.value.getBoundingClientRect().width/500;
+	scaleY.value = canvas1.value.getBoundingClientRect().height/500;
+	ctx1.value = canvas1.value.getContext("2d");
+	canvas2.value = document.getElementById("canvas2");
+	ctx2.value = canvas2.value.getContext("2d");
+	ctx1.value.translate(0.5,0.5);
+	ctx2.value.translate(0.5,0.5);
 
 	// Set default stroke color
-	ctx.value.strokeStyle = colors.value[0];
-	ctx.value.fillStyle = colors.value[0];
+	ctx1.value.strokeStyle = colors.value[0];
+	ctx1.value.fillStyle = colors.value[0];
+	ctx2.value.strokeStyle = colors.value[0];
+	ctx2.value.fillStyle = colors.value[0];
 
-	// Resize canvas
-	canvas.value.height = window.innerHeight * 0.6;
-	canvas.value.width = window.innerWidth * 0.8;
+	canvas2.value.style.left = "-200%";
+	canvas1.value.width = 500*scaleX.value/scaleY.value;
+	canvas2.value.width = 500*scaleX.value/scaleY.value;
+	canvas1.value.height = 500;
+	canvas2.value.height = 500;
+	scaleX.value = canvas1.value.getBoundingClientRect().width/canvas1.value.width;
 });
 </script>
 
@@ -141,10 +210,6 @@ h2 {
 }
 
 canvas {
-	display: block;
-	background-color: #ffffff;
-	border: 2px solid #333;
-	border-radius: 5px;
 	cursor: crosshair;
 }
 
@@ -206,5 +271,23 @@ canvas {
 
 .clear-button:hover {
 	background-color: #444;
+}
+
+#wrapper{
+	position:relative;
+	width:100%;
+	height:400px;
+}
+#canvas1{
+	position:absolute; top:0px; left:0px;
+	border:1px solid black;
+	width:100%;
+	height:100%;
+}
+#canvas2{
+	position:absolute; top:0px; left:0px;
+	/* border:1px solid red; */
+	width:100%;
+	height:100%;
 }
 </style>
